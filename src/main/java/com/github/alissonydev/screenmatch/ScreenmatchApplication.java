664cf19e08774dd26b63465717f713a4e65208fc
alissonydev <alissonydev@outlook.com>
2026-01-04 23:44:28 -0300

    Exibindo temporadas e episodios

--- src/main/java/com/github/alissonydev/screenmatch/ScreenmatchApplication.java
+++ src/main/java/com/github/alissonydev/screenmatch/ScreenmatchApplication.java
@@ -1,17 +1,9 @@
 package com.github.alissonydev.screenmatch;
 
-import com.github.alissonydev.screenmatch.models.DadosEpisodio;
-import com.github.alissonydev.screenmatch.models.DadosSerie;
-import com.github.alissonydev.screenmatch.models.DadosTemporada;
 import com.github.alissonydev.screenmatch.principal.Principal;
-import com.github.alissonydev.screenmatch.services.ConsumoApi;
-import com.github.alissonydev.screenmatch.services.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
@@ -24,62 +16,6 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-
-        String url = "https://www.omdbapi.com/?t=gilmore+girls&Seanson=1&apikey=";
-
-        final ConsumoApi consumoApi = new ConsumoApi();
-        var json = consumoApi.obterDados(url + YOUR_API_KEY);
-        System.out.println("-----------------------------------");
-        System.out.println(json);
-        System.out.println("-----------------------------------");
-
-        final String coffee_img = "https://coffee.alexflipnote.dev/random.json";
-        json = consumoApi.obterDados(coffee_img);
-        System.out.println(json);
-
-        url = "https://www.omdbapi.com/?t=gilmore+girls&apikey=";
-        json = consumoApi.obterDados(url + YOUR_API_KEY);
-        System.out.println(url + YOUR_API_KEY);
-        System.out.println(json);
-
-        final ConverteDados conversor = new ConverteDados();
-        DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
-        System.out.println(dados);
-
-        url = "https://www.omdbapi.com/?t=gilmore+girls&season=1&episode=2&apikey=";
-        json = consumoApi.obterDados(url + YOUR_API_KEY);
-        DadosEpisodio episodio = conversor.obterDados(json, DadosEpisodio.class);
-        System.out.println("-----------------------------------");
-        System.out.println(episodio);
-        System.out.println();
-        System.out.println();
-
-
-
-        List<DadosTemporada> temporadas = new ArrayList<>();
-
-        for (int i = 1; i <= dados.totalTemporadas(); i++) {
-            url = "https://www.omdbapi.com/?t=gilmore+girls&season=" + i +"&apikey=";
-            json = consumoApi.obterDados(url + YOUR_API_KEY);
-            DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
-            System.out.println(dadosTemporada);
-            temporadas.add(dadosTemporada);
-        }
-
-        System.out.println();
-        System.out.println("-----------------------------------");
-
-        temporadas.forEach(System.out::println);
-
-        System.out.println();
-        System.out.println("-----------------------------------");
-
-        temporadas.forEach(x -> {
-            System.out.println("Temporada: " + x.numero());
-            x.episodios().forEach(epis ->
-                System.out.println("Episódio: " + epis.numero() + " - " + epis.titulo())
-            );
-        });
 
         final Principal principal = new Principal();
         principal.exibeMenu();
--- src/main/java/com/github/alissonydev/screenmatch/principal/Principal.java
+++ src/main/java/com/github/alissonydev/screenmatch/principal/Principal.java
@@ -1,29 +1,86 @@
 package com.github.alissonydev.screenmatch.principal;
 
+import com.github.alissonydev.screenmatch.models.DadosEpisodio;
+import com.github.alissonydev.screenmatch.models.DadosSerie;
+import com.github.alissonydev.screenmatch.models.DadosTemporada;
 import com.github.alissonydev.screenmatch.services.ConsumoApi;
+import com.github.alissonydev.screenmatch.services.ConverteDados;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Principal {
 
+    final Scanner leitura = new Scanner(System.in);
     final ConsumoApi consumoApi = new ConsumoApi();
+    final ConverteDados conversor = new ConverteDados();
     private static final String URL = "https://www.omdbapi.com/?t=";
     private static final String API_KEY = System.getenv("YOUR_API_KEY");
-    //gilmore+girls&apikey
 
     public void exibeMenu() {
 
         System.out.println();
         System.out.println("=== MENU ===");
-        System.out.println("Digite um nome de um filme");
-        final Scanner leitura = new Scanner(System.in);
+        System.out.println("Digite um nome de um filme para busca:");
+
         final String nomeSerie = leitura.nextLine();
-        String urlCompleta = URL + nomeSerie.replace(" ", "+") + "&apikey=" + API_KEY;
-        System.out.println(urlCompleta);
 
+        var json = consumoApi
+                .obterDados(URL + nomeSerie.replace(" ", "+") + "&apikey=" + API_KEY);
 
-        var json = consumoApi.obterDados(urlCompleta);
-        System.out.println(json);
+        DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+        System.out.println(dados);
+
+        System.out.println();
+        System.out.println("WAIT... Buscando temporadas...");
+
+        List<DadosTemporada> temporadas = new ArrayList<>();
+
+        for (int i = 1; i <= dados.totalTemporadas(); i++) {
+            var url = "https://www.omdbapi.com/?t=gilmore+girls&season=" + i +"&apikey=";
+            json = consumoApi.obterDados(url + API_KEY);
+            DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
+        //    System.out.println(dadosTemporada);
+            temporadas.add(dadosTemporada);
+        }
+        System.out.println("-----------------------------------");
+        temporadas.forEach(System.out::println);
+
+        System.out.println();
+        System.out.println("=== LISTA DE TEMPORADAS E EPISÓDIOS ===");
+        temporadas.forEach(x -> {
+            System.out.println("Temporada: " + x.numero());
+            x.episodios().forEach(epis ->
+                System.out.println("Episódio: " + epis.numero() + " - " + epis.titulo())
+            );
+        });
+
+        System.out.println();
+        System.out.println("--------------------------------------");
+
+        for (int i = 0; i < dados.totalTemporadas(); i++) {
+            List<DadosEpisodio> episodiosTemporada = temporadas.get(i).episodios();
+            for (int j = 0; j < episodiosTemporada.size(); j++) {
+                System.out.println(episodiosTemporada.get(j).titulo());
+               // DadosEpisodio episodio = episodiosTemporada.get(j);
+//                System.out.println("Temporada " + (i + 1) + " - Episódio " + episodio.numero() +
+//                        ": " + episodio.titulo() + " | Avaliação: " + episodio.avaliacao() +
+//                        " | Data de Lançamento: " + episodio.dataLancamento());
+            }
+        }
+
+        // usando lambda
+        System.out.println();
+        System.out.println("=== DETALHES DOS EPISÓDIOS ===");
+         temporadas.forEach(temporada -> {
+             temporada.episodios().forEach(episodio -> {
+                 System.out.println("Temporada " + temporada.numero() + " - Episódio " + episodio.numero() +
+                         ": " + episodio.titulo() + " | Avaliação: " + episodio.avaliacao() +
+                         " | Data de Lançamento: " + episodio.dataLancamento());
+             });
+         });
+
 
         // "https://www.omdbapi.com/?t=gilmore+girls&season=6585022c"
 //        System.out.println("Digite o nome da série ou filme que deseja buscar:");
